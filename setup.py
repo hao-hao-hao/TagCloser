6ca63e24185fb6a86605ec5257a2ff6b114bbc44
Hao <haleluohao@gmail.com>
2018-02-03 22:38:20 +0000

    first release

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='tagcloser',
+      version='1.3.0',
+      url='https://github.com/Hao-Luo/TagCloser',
+      keywords='html htmltag excerpt',
+      description='This is to close unclosed html tag in html file, mainly for generating the excerpt html.',
+      author='Hao Luo',
+      author_email='hao@sporule.com',
+      license='GPL',
+      packages=['tagcloser'],
+      zip_safe=False)
